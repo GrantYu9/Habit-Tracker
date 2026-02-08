fc9a8ee4879bc0fbd7071bd05c22176df159eb07
Grant Yu <gyu09@students.cs.ubc.ca>
2026-02-08 11:39:31 -0800

    Finished documentation and specification for Folder class

--- src/main/model/Organization/Folder.java
+++ src/main/model/Organization/Folder.java
@@ -1,6 +1,47 @@
 package model.organization;
 
-// !!!
+import java.util.ArrayList;
+import java.util.List;
+
+// A container for folders and pages
 public class Folder {
-    
+    private String title;
+
+    private List<Folder> folders;
+    private List<Page> pages;
+
+    /*
+    REQUIRES:
+        title has at least one character
+    EFFECTS: initializes Page such that 
+        this.title = title, with surrounding whitespace removed
+
+        folders = new ArrayList<>()
+        pages = new ArrayList<>()
+     */
+    public Folder(String title) {
+        // !!!
+    }
+
+    public void addToFolder(Folder folder) {
+        // !!!
+    }
+
+    public void addToFolder(Page page) {
+        // !!!
+    }
+
+    // REQUIRES: title has at least one character
+    // EFFECTS: this.title = title, with surrounding whitespace trimmed
+    public void setTitle(String title) {
+        // !!!
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public List<Page> getPages() {
+        return pages;
+    }
 }
