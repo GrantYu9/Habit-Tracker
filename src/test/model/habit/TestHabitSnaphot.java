f4b2182b425f97f721f7f6d8451b302c7a25a495
Grant Yu <gyu09@students.cs.ubc.ca>
2026-02-11 14:48:17 -0800

    Finished tests for HabitSnapshot class

--- src/test/model/habit/TestHabitSnaphot.java
+++ src/test/model/habit/TestHabitSnaphot.java
@@ -1,18 +1,41 @@
 package model.habit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
+
+import model.habit.Habit.ProgressType;
 
 public class TestHabitSnaphot {
-    @BeforeEach
-    void beforeEach() {
-        // !!!
+    private HabitSnapshot testHabitSnapshotA;
+    private HabitSnapshot testHabitSnapshotB;
+
+    @Test
+    void testConstructorA() {
+        testHabitSnapshotA = new HabitSnapshot(1, 2, 0, 50, 0, 1, ProgressType.UNDERDONE, "breaks");
+
+        assertEquals(1, testHabitSnapshotA.getCurrentAmount());
+        assertEquals(2, testHabitSnapshotA.getGoal());
+        assertEquals(0, testHabitSnapshotA.getOverloadAmount());
+        assertEquals(50, testHabitSnapshotA.getProgressPercentage());
+        assertEquals(0, testHabitSnapshotA.getStartingAmount());
+        assertEquals(1, testHabitSnapshotA.getStepAmount());
+        assertEquals(ProgressType.UNDERDONE, testHabitSnapshotA.getProgressType());
+        assertTrue(testHabitSnapshotA.getUnit().equals("breaks"));
     }
 
     @Test
-    void testConstructor() {
-        // !!!
+    void testConstructorB() {
+        testHabitSnapshotB = new HabitSnapshot(6, 4, 2, 100, 2, 2, ProgressType.OVERLOADED, "study sessions");
+
+        assertEquals(6, testHabitSnapshotB.getCurrentAmount());
+        assertEquals(4, testHabitSnapshotB.getGoal());
+        assertEquals(2, testHabitSnapshotB.getOverloadAmount());
+        assertEquals(100, testHabitSnapshotB.getProgressPercentage());
+        assertEquals(2, testHabitSnapshotB.getStartingAmount());
+        assertEquals(2, testHabitSnapshotB.getStepAmount());
+        assertEquals(ProgressType.OVERLOADED, testHabitSnapshotB.getProgressType());
+        assertTrue(testHabitSnapshotB.getUnit().equals("study sessions"));
     }
 }
