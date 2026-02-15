113af3d9a12a88b37f32e922d20ad5af6b5430b4
Grant Yu <gyu09@students.cs.ubc.ca>
2026-02-14 16:39:25 -0800

    Implement AllTagPagesPage

--- src/main/model/Organization/centralization/AllTagPagesPage.java
+++ src/main/model/Organization/centralization/AllTagPagesPage.java
@@ -1,5 +1,6 @@
 package model.organization.centralization;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import model.organization.specialpages.TagPage;
@@ -10,14 +11,14 @@ public class AllTagPagesPage {
 
     // EFFECTS: Instantiates AllTagPages such that tagPages = new ArrayList
     public AllTagPagesPage() {
-        // !!!
+        tagPages = new ArrayList<>();
     }
 
     // REQUIRES: tagPage is not already in tagPages
     // MODIFIES: this
     // EFFECTS: Adds tagPage to tagPages
     public void addToTagPages(TagPage tagPage) {
-        // !!!
+        tagPages.add(tagPage);
     }
 
     public List<TagPage> getTagPages() {
