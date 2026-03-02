3389e4434902fb4ef7d545c292fd1b86782f405a
Grant Yu <gyu09@students.cs.ubc.ca>
2026-03-01 17:12:44 -0800

    Remove redundancy in exceptions
    Exceptions had constructors which allowed a string to be taken in as an
    operand, but had no use case. Was also hurting code coverage score for
    minimal reason.

--- src/main/model/exceptions/EmptyLastTimeFileException.java
+++ src/main/model/exceptions/EmptyLastTimeFileException.java
@@ -4,11 +4,6 @@ public class EmptyLastTimeFileException extends Exception {
     
     // EFFECTS: Instantiates the exception without a message
     public EmptyLastTimeFileException() {
-
-    }
-
-    // EFFECTS: Instantiates the exception with a message
-    public EmptyLastTimeFileException(String message) {
-        super(message);
+        
     }
 }
--- src/main/model/exceptions/HabitNotFoundException.java
+++ src/main/model/exceptions/HabitNotFoundException.java
@@ -3,11 +3,6 @@ package model.exceptions;
 public class HabitNotFoundException extends Exception {
     // EFFECTS: Instantiates the exception without a message
     public HabitNotFoundException() {
-
-    }
-
-    // EFFECTS: Instantiates the exception with a message
-    public HabitNotFoundException(String message) {
-        super(message);
+        
     }
 }
